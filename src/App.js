1dabbe43fbc9361720ad9d515e72c33eab54b805
khuong-truong <khuong.truong@codecomplete.jp>
2022-02-21 13:45:40 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Khuong Truong
+          Khuong Truong hihi
         </p>
         <a
           className="App-link"
